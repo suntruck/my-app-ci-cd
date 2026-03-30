f6f5434a9d34cbc9809cfce5d722c36b7c10be8f
i321872 <dongdong.zheng@sap.com>
2026-03-30 18:03:27 +0800

    use command ci

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          The changes will be included automatically using the GitHub Actions CI/CD pipeline to deploy to Vercel.
         </p>
         <a
           className="App-link"
